bc9f2a09981b52cf60055938f68def3be11f2bf7
jrfm <moesch.jonas@protonmail.com>
2023-10-09 21:59:42 +0200

    add one working taco card

--- pages/Tacos.jsx
+++ pages/Tacos.jsx
@@ -3,20 +3,50 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import styled from "styled-components";
+import Card from "react-bootstrap/Card";
 
 const Section = styled.section`
   padding: 6rem 0;
 `;
 
 const H2 = styled.h2`
-  font-size: 1.5rem;
-  font-weight: 700;
-  margin-bottom: 0;
+  font-size: 2.5rem;
+  margin-top: 0;
+  margin-bottom: 1rem;
 `;
 
-const H3 = styled.h3``;
+const H3 = styled.h3`
+  font-size: 1rem;
+  font-weight: 400;
+  font-style: italic;
+  margin-bottom: 4rem;
+`;
+
+const tacos = [
+  {
+    img: "../public/images/al-pastor.jpg",
+    name: "Al Pastor",
+    contents:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  },
+];
 
 export function Tacos() {
+  const allTacos = tacos.map((taco) => (
+    <>
+      {" "}
+      <Col>
+        {" "}
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={taco.img} />
+          <Card.Body>
+            <Card.Title>{taco.name}</Card.Title>
+            <Card.Text>{taco.contents}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    </>
+  ));
   return (
     <Section>
       <Container>
@@ -24,7 +54,7 @@ export function Tacos() {
           <H2>Our Tacos</H2>
           <H3>Lorem ipsum dolor sit amet consectetur.</H3>
         </div>
-        <Row></Row>
+        <Row>{allTacos}</Row>
       </Container>
     </Section>
   );
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigation } from "../components/navigation/Navigation";
 import { Hero } from "../pages/Hero";
 import { Foodtruck } from "../pages/FoodTruck";
+import { Tacos } from "../pages/Tacos";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navigation />
       <Hero />
       <Foodtruck />
+      <Tacos />
     </>
   );
 }
